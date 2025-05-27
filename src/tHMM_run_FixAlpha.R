81ca4e5cf906737e040b7f1a021e46ee80f824be
Giovanni-Romano <giovanni.romano4@phd.unibocconi.it>
2025-05-27 12:37:39 +0200

    minor fix on run with fixed alpha

--- src/tHMM_run_FixAlpha.R
+++ src/tHMM_run_FixAlpha.R
@@ -10,7 +10,6 @@ Rcpp::sourceCpp('src/ArgientoPaci/code/gibbs_utility.cpp')
 Rcpp::sourceCpp('src/ArgientoPaci/code/hyperg2.cpp')
 
 ghe = readRDS("data/rds/dataGHE.RDS") %>%
-  filter(Year <= 2009) %>% 
   unite("ID", c(CountryN, Sex), remove = FALSE)
 ghe.lab = unique(ghe %>% select(c("Age", "CauseS"))) %>% 
   group_by(Age) %>% 
@@ -30,7 +29,7 @@ p = dim(ghe.array)[2]
 u = rep(4, p)
 v = rep(0.25, p)
 
-eta = 0.45
+eta = 0.4
 {
   probs_gnedin = HGnedin(n, 1:n, gamma = eta)
   cat("prior expected ncl with eta ", eta, " is ", round(sum(1:n*probs_gnedin), 2), "\n", sep = "")
@@ -43,7 +42,7 @@ printstep = 500
 nclinit = 100
 urntype = "Gnedin"
 
-out1 = tHMM_gibbs(
+out = tHMM_gibbs(
   Y = ghe.array,
   m = m,
   # Parameters for the likelihood-specific local parameters
@@ -59,7 +58,7 @@ out1 = tHMM_gibbs(
   ctr_mcmc = list(seed = seedrun, nburnin = nburn, nchain = nchain, print_step = printstep, 
                   ncl.init = nclinit, verbose = "1"),
   # Control parameters for result storing
-  ctr_save = list(save = FALSE, filepath = "output/tHMM/",
+  ctr_save = list(save = TRUE, filepath = "output/tHMM/",
                   filename = paste("res_GnedinFixAlpha", seedrun, ".RDS", sep = "")),
   ctr_alpha = list(fix_alpha.flag = TRUE, fix_alpha.value = 1e-3)
 )
