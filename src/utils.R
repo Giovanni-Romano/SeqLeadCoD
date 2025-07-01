9236bf6dc644e8ece697d7cadde17766f1b0864d
Giovanni-Romano <giovanni.romano4@phd.unibocconi.it>
2025-07-01 19:29:32 +0200

    changes to refsort and seqsort

--- src/utils.R
+++ src/utils.R
@@ -34,7 +34,7 @@ dimsort = function(u){
 }
 
 # Sort the labels of a partition by matching the labels of a reference partition
-refsort = function(u, v){
+refsort.cri = function(u, v){
   # Partition dimensions
   nu = max(u)
   nv = max(v)
@@ -83,11 +83,75 @@ refsort = function(u, v){
   return(out)
 }
 
+# Sort the labels of a partition by matching the labels of a reference partition
+refsort = function(u, v){
+  # Partition dimensions
+  nu = max(u)
+  nv = max(v)
+  
+  # Membership matrices
+  U = vec2mat(u)
+  V = vec2mat(v)
+  
+  # Matrix of cross-membership counts
+  mat = crossprod(V, U)
+  tab = table(u)
+  
+  # Used and unused labels, and empty ordering vector
+  used = c()
+  unused = c(1:nu)
+  order = rep(NA, length = nu)
+  
+  # Order initialization
+  h = 1
+  idx = which.max(mat[h, ])
+  used = c(used, idx)
+  unused = setdiff(unused, idx)
+  order = idx
+  
+  # Sorting loop
+  for(h in 2:nu){
+    # Positions of the most frequent co-clustering values in decreasing order
+    if (h <= nv) {
+      tmp = order(mat[h, ], decreasing = TRUE)
+    } else {
+      tmp = order(tab, decreasing = TRUE)
+    }
+    
+    idx = tmp[1]
+    
+    if (idx %in% used){
+      next
+    } else {
+      # Update used and unused labels
+      used = c(used, idx)
+      unused = setdiff(unused, idx)
+      # Set the h-th entry of the order vector
+      order = c(order, idx)
+    }
+  }
+  
+  order = c(order, unused)
+  
+  # Re-order the columns of the membership matrix and
+  out = mat2vec(U[, order])
+  
+  # Return the re-labeled partition
+  return(out)
+}
+
 # Sort the labels of a sequence of partitions by sequential applying refsort 
-seqsort = function(partition, reference = TRUE){
-  # Sort all the partitions by cluster dimension 
-  out = apply(partition, 2, dimsort)
-  dimnames(out) = dimnames(partition)
+seqsort = function(partition, reference = TRUE, dimsort = TRUE){
+  
+  if (dimsort){
+    # Sort all the partitions by cluster dimension 
+    out = apply(partition, 2, dimsort)
+    dimnames(out) = dimnames(partition)
+  } else {
+    # If not sorting by cluster dimension, just copy the input
+    out = partition
+  }
+  
   # Relabel each partition by sequential reference sorting 
   if(reference){
     for(t in 2:ncol(out)){
@@ -96,6 +160,7 @@ seqsort = function(partition, reference = TRUE){
   }
   return(out)
 }
+
 
 moda = function(x){
   tx = table(x)
