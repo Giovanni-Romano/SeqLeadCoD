30f018476814f94e93f6cfea644e1a7a157abdef
Giovanni-Romano <giovanni.romano4@phd.unibocconi.it>
2025-09-17 15:33:28 +0200

    small addition to script to compare runs

--- src/tHMM_compare_runs.R
+++ src/tHMM_compare_runs.R
@@ -1,4 +1,6 @@
-library(tidyverse)
+suppressPackageStartupMessages({
+  library(tidyverse)
+})
 
 sex = "female"
 OUTFOLDER = paste0("output/tHMM/", sex)
@@ -7,10 +9,28 @@ runs = list.files(OUTFOLDER, pattern = "PPE", full.names = F) %>% str_sub(5, 9)
 nruns = length(runs)
 years = 2000:2021
 nyears = length(years)
+causes = readr::read_csv2("data/raw/causes.csv") %>% select(CauseS) %>% unique() %>% unlist() %>% unname() %>% sort() 
+ncauses = length(causes)
+
+palette = c(pals::alphabet(26)[-(4:5)], pals::alphabet2(26), pals::glasbey(32))[1:ncauses]
+names(palette) = sort(table(readRDS("data/rds/dataGHE.RDS")$CauseS %>% as.character()), decreasing = T) %>% names
+
+
+# Save plots? ----
+SAVE = FALSE
+
+# Load data ----
+if (sex == "female"){
+  ghe = readRDS("data/rds/GHEdf_female.RDS")
+} else if (sex == "male"){
+  ghe = readRDS("data/rds/GHEdf_male.RDS")
+} else {
+  stop("Wrong sex value")
+}
 
 # Load PPE ----
 PPE = abind::abind(lapply(list.files(OUTFOLDER, pattern = "PPE", full.names = T), readRDS), along = 3)
-dimnames(PPE)[[3]] = runs
+dimnames(PPE) = list(Country = rownames(PPE), Year = colnames(PPE), Run = runs)
 
 # NMI PPE across runs ----
 NMI = array(NA, dim = c(nruns, nruns, nyears))
@@ -39,10 +59,12 @@ plt_NMI = NMI %>% reshape2::melt() %>%
   theme(legend.position = "NULL", 
         axis.text.x = element_text(angle = 90, vjust = 0.5))
 
-width = 12; height = 6; zoom = 1 
-ggsave(filename = "NMI.pdf", path = IMGFOLDER,
-       plot = plt_NMI,
-       width = width*zoom, height = height*zoom)
+if (SAVE){
+  width = 12; height = 6; zoom = 1 
+  ggsave(filename = "NMI.pdf", path = IMGFOLDER,
+         plot = plt_NMI,
+         width = width*zoom, height = height*zoom)
+}
 
 # Number of clusters ----
 ncl = apply(PPE, c(2, 3), max)
@@ -54,10 +76,12 @@ plt_ncl = ncl %>% as.data.frame() %>%
   geom_line() + geom_point() + 
   theme_bw()
 
-width = 12; height = 6; zoom = 1 
-ggsave(filename = "ncl.pdf", path = IMGFOLDER,
-       plot = plt_ncl,
-       width = width*zoom, height = height*zoom)
+if (SAVE){
+  width = 12; height = 6; zoom = 1 
+  ggsave(filename = "ncl.pdf", path = IMGFOLDER,
+         plot = plt_ncl,
+         width = width*zoom, height = height*zoom)
+}
 
 
 # Posterior similarity matrices ----
@@ -88,8 +112,53 @@ for (y in seq_along(years)){
                        axis.title = element_blank())
 }
 
-width=10; height = 3; zoom = 1
-ggsave(filename = "PSM.pdf", path = IMGFOLDER,
-       plot = gridExtra::marrangeGrob(plt_PSM, nrow = 1, ncol = 1, top = NULL), 
-       height = height*zoom, width = width*zoom)
+if (SAVE){
+  width=10; height = 3; zoom = 1
+  ggsave(filename = "PSM.pdf", path = IMGFOLDER,
+         plot = gridExtra::marrangeGrob(plt_PSM, nrow = 1, ncol = 1, top = NULL), 
+         height = height*zoom, width = width*zoom)
+}
 
+# Plot of data split by cluster ----
+ghePPE = ghe %>% 
+  right_join(PPE %>% reshape2::melt(value.name = "Cluster"),
+             by = c("Year", "CountryN" = "Country"),
+             relationship = "many-to-many")
+
+ghePPE %>% filter(Year == 2000, Run %in% c("20010", "20019")) %>% 
+  select(-c(Age, CauseC:CauseT)) %>% distinct() %>% 
+  pivot_wider(names_from = "Run", names_prefix = "Run", values_from = Cluster) %>%
+  mutate(Clust.diff = if_else(Run20010 != Run20019, 1, 0)) %>% 
+  filter(Clust.diff == 1) %>% arrange(Run20010) %>%  View()
+
+
+
+for (y in seq_along(years)){
+  yyy = years[y] %>% as.character()
+  tmp = list()
+  
+  for (r in seq_along(runs)){
+    
+    run = runs[r]
+    
+    
+    tmp[[r]] = ghePPE %>% 
+      filter(Year == 2000, Run == run) %>% 
+      ggplot(aes(x = CountryN, y = Age, fill = CauseS)) + 
+      geom_raster() +
+      facet_grid(cols = vars(Cluster), scales = "free", space = "free") + 
+      scale_fill_manual(values = palette) + 
+      guides(fill = guide_legend(nrow = 2)) +
+      theme(legend.position = "top", 
+            axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+      labs(title = paste0("Run seed ", run))
+  }
+  
+  if (SAVE){
+    width = 32; height = 8; zoom = 1
+    ggsave(filename = paste0("dataPPE_", yyy, ".pdf"),
+           path = IMGFOLDER, 
+           plot = gridExtra::marrangeGrob(tmp, nrow = 1, ncol = 1, top = NULL),
+           width = width*zoom, height = height*zoom)
+  }
+}
