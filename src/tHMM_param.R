28428699aa9ef4d4c4c49102068a5b9bda376f64
Giovanni-Romano <giovanni.romano4@phd.unibocconi.it>
2025-11-12 18:25:07 +0100

    minor changes to adapt script to run "conditional" Gibbs to sample parameters

--- src/tHMM_param.R
+++ src/tHMM_param.R
@@ -2,6 +2,7 @@ options(warn = 1)
 suppressPackageStartupMessages(library(tidyverse))
 library(abind)
 library(RcppArmadillo)
+source("src/utils.R")
 source("src/tHMM_utils.R")
 source("src/tHMM_updates.R")
 source("src/tHMM_gibbs.R")
@@ -9,14 +10,20 @@ Rcpp::sourceCpp('src/cpp_utils.cpp')
 Rcpp::sourceCpp('src/ArgientoPaci/code/gibbs_utility.cpp')
 Rcpp::sourceCpp('src/ArgientoPaci/code/hyperg2.cpp')
 
-PPE = readRDS("output/tHMM/PPE_Gnedin20019.RDS")
-res = readRDS("output/tHMM/res_Gnedin20019.RDS")
-Y = res$input$Y
-m = res$input$m
-par_lik = res$input$par$par_likelihood
+
+SEX = "female"
+SEED = 20019
+NAME = paste0("Gnedin", SEED)
+OUTFOLDER = paste0("output/tHMM/5yrs/PT/", SEX, "/")
+res = readRDS(paste0(OUTFOLDER, "res_", NAME, ".RDS"))
+PPE = readRDS(paste0(OUTFOLDER, "PPE_all.RDS"))[[as.character(SEED)]]
+
+Y = res$Y
+m = res$m
+par_lik = res$par$par_likelihood
 
 
-seedrun = 20019
+seedrun = SEED
 urntype = "Gnedin"
 nburn = 5000
 nchain = 15000
@@ -27,41 +34,24 @@ out = tHMM_gibbs_parameters(
   par_likelihood = par_lik,
   ctr_mcmc = list(seed = seedrun, nburnin = nburn, nchain = nchain, print_step = printstep, 
                   verbose = "1"),
-  ctr_save = list(save = TRUE, filepath = "output/tHMM/",
+  ctr_save = list(save = TRUE, filepath = OUTFOLDER,
                   filename = paste("param_", urntype, seedrun, ".RDS", sep = ""))
 )
 
 
-# mu.mode = lapply(out$mu, function(x) apply(x, c(1, 2), moda))
-# str(mu.mode)
-# 
-# 
-# ghe = readRDS("data/rds/dataGHE.RDS") %>%
-#   unite("ID", c(CountryN, Sex), remove = FALSE)
-# ghe.lab = unique(ghe %>% select(c("Age", "CauseS"))) %>% 
-#   group_by(Age) %>% 
-#   mutate(CauseI = as.integer(factor(CauseS)))
-# 
-# mu.mode_df = mu.mode %>% reshape2::melt() %>% 
-#   rename(Cluster = Var1, Age = Var2, CauseI = value, Year = L1) %>%
-#   left_join(., ghe.lab %>% mutate(Age = as.integer(Age)), by = c("Age", "CauseI"))
-# 
-# 
-# plt = mu.mode_df %>% 
-#   filter(Year == 1) %>%
-#   ggplot(aes(x = Age, y = Cluster, fill = CauseS)) +
-#   geom_raster() + 
-#   facet_wrap(~Year) + 
-#   geom_text(aes(label = CauseS), size = 2) +
-#   scale_fill_manual(values = palette) +
-#   theme(legend.position = "none",
-#         panel.grid = element_blank(),
-#         plot.background = element_rect(fill = "white"),
-#         panel.background = element_rect(fill = "white"),
-#         axis.title.y = element_blank(),
-#         axis.ticks.y = element_blank(),
-#         axis.text.y = element_blank(),
-#         axis.text.x = element_text(angle = 45, hjust = 1))
-# 
-# sigma.mean = lapply(out$sigma, function(x) apply(x, c(1, 2), mean))
-# 
+mu.mode = lapply(out$mu, function(x) apply(x, c(1, 2), moda))
+str(mu.mode)
+
+
+ghe = readRDS("data/rds/dataGHE.RDS") %>%
+  unite("ID", c(CountryN, Sex), remove = FALSE)
+ghe.lab = unique(ghe %>% select(c("Age", "CauseS"))) %>%
+  group_by(Age) %>%
+  mutate(CauseI = as.integer(factor(CauseS)))
+
+mu.mode_df = mu.mode %>% reshape2::melt() %>%
+  rename(Cluster = Var1, Age = Var2, CauseI = value, Year = L1) %>%
+  left_join(., ghe.lab %>% mutate(Age = as.integer(Age)), by = c("Age", "CauseI"))
+
+
+saveRDS(mu.mode_df, paste0(OUTFOLDER, "muestdf_", SEED, ".RDS"))
