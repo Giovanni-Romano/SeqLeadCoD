2f8d81fab9c7d3d527ec4b5b6bdf706969a66fb2
Giovanni <giovanni@Giovannis-MacBook-Pro.local>
2025-12-23 20:21:15 +0100

    script to run model on data with drug together with self-harm

---
+++ src/tHMM_run_Gnedin_v2.R
@@ -0,0 +1,98 @@
+options(warn = 1)
+suppressPackageStartupMessages(library(tidyverse))
+library(abind)
+library(RcppArmadillo)
+source("src/tHMM_utils.R")
+source("src/tHMM_updates.R")
+source("src/tHMM_gibbs.R")
+Rcpp::sourceCpp('src/cpp_utils.cpp')
+Rcpp::sourceCpp('src/ArgientoPaci/code/gibbs_utility.cpp')
+Rcpp::sourceCpp('src/ArgientoPaci/code/hyperg2.cpp')
+
+cmline = c("female", 666) #commandArgs(trailingOnly = TRUE)
+cat("Arguments from command line:", cmline, sep = " ")
+sex = cmline[1]
+seed = cmline[2] %>% as.integer
+
+if (sex == "female"){
+  ghe = readRDS("data/rds/GHEdf_female_5yrs_v2.RDS")
+} else if (sex == "male"){
+  ghe = readRDS("data/rds/GHEdf_male_5yrs_v2.RDS")
+} else {
+  stop("Wrong sex value")
+}
+
+ghe.lab = unique(ghe %>% select(c("Age", "CauseS"))) %>% 
+  group_by(Age) %>% 
+  mutate(CauseI = as.integer(factor(CauseS)))
+m = ghe.lab %>% group_by(Age) %>% summarise(m = n()) %>% pull(m)
+
+ghe.w = ghe %>% 
+  left_join(ghe.lab, by = c("Age", "CauseS")) %>% 
+  select(c("Country", "Age", "Year", "CauseI"))  %>% 
+  arrange(Country, Age, Year)   %>% 
+  pivot_wider(names_from = Age, values_from = CauseI)
+ghe.array = abind(map(split(ghe.w, ghe.w$Year), 
+                      \(x) x %>% select(-Year) %>% column_to_rownames("Country")), along = 3)
+
+n = dim(ghe.array)[1]
+p = dim(ghe.array)[2]
+.T = dim(ghe.array)[3]
+u = rep(4, p)
+v = rep(0.25, p)
+
+C_init = readRDS(paste0("output/tHMM/5yrs/PPE_", sex, ".RDS"))[[1]]
+rownames(C_init) = rownames(ghe.array)
+
+eta_values = seq(0.45, 0.55, by = 0.01)
+exp_ncl = sapply(eta_values, function(e) {
+  probs_gnedin = HGnedin(n, 1:n, gamma = e)
+  round(sum(1:n*probs_gnedin))
+})
+cbind(eta = eta_values, exp_ncl = exp_ncl)
+eta = 0.55
+{
+  probs_gnedin = HGnedin(n, 1:n, gamma = eta)
+  cat("prior expected ncl with eta ", eta, " is ", round(sum(1:n*probs_gnedin), 2), "\n", sep = "")
+}
+
+seedrun = seed
+nburn = 500
+nchain = 2500
+printstep = 50
+nclinit = 50
+urntype = "Gnedin"
+SAVE = TRUE
+
+# PT parameters
+.nrep = 8
+.ntry = floor(.nrep / 2)
+.start_swap = 1
+.adaptive = TRUE
+.deterministic = TRUE
+.prob_target = 0.65
+
+out = tHMM_gibbs_adaptive_PT(
+  Y = ghe.array,
+  m = m,
+  C_init = C_init,
+  # Parameters for the likelihood-specific local parameters
+  par_likelihood = list(u = u, # First hyperparam HIG
+                        v = v # Second hyperparam HIG
+  ),
+  # Parameters for the tRPM process prior
+  par_tRPM = list(a_alpha = 1, b_alpha = 1, # Beta prior on alpha
+                  urn_type = urntype, # Type of urn process
+                  eta = eta # Gnedin parameter
+  ),
+  # Control parameters for MCMC settings
+  ctr_mcmc = list(seed = seedrun, nburnin = nburn, nchain = nchain, print_step = printstep, 
+                  ncl.init = nclinit, verbose = "1",
+                  parallel = FALSE, nthreads = .nrep),
+  # Control parameters for Parallel Tempering
+  ctr_swap = list(nrep = .nrep, ntry = .ntry, start = .start_swap, deterministic = .deterministic, 
+                  adaptive = .adaptive, prob_target = .prob_target, swap_step = 5),
+  # Control parameters for result storing
+  ctr_save = list(save = SAVE, filepath = paste0("output/tHMM/5yrs/PT", sex, "/"),
+                  filename = paste("res_", urntype, seedrun, ".RDS", sep = "")),
+)
